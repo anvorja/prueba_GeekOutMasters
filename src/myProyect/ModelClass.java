2624d7d72ba8d0a5c58be09e5c117c52f0ac43c5
anvorja <andres.vorja.vorja@gmail.com>
2022-01-11 10:54:59 -0500

    updated ModelClass with constructor and toString method

--- src/myProyect/ModelClass.java
+++ src/myProyect/ModelClass.java
@@ -1,4 +1,14 @@
 package myProyect;
 
 public class ModelClass {
+
+    //Constructor
+    public Model(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "ModelClass{}";
+    }
 }
