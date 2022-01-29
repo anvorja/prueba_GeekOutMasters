3a89298154f8c9040fbeb97fd36a826e501b7fd5
anvorja <andres.vorja.vorja@gmail.com>
2022-01-29 13:10:50 -0500

    ramas

--- src/myProyect/ModelClass.java
+++ src/myProyect/ModelClass.java
@@ -10,11 +10,11 @@ import java.util.ArrayList;
 public class ModelClass {
 
     private Dado dado1, dado2, dado3, dado4, dado5, dado6, dado7, dado8, dado9, dado10;
-    private ArrayList <Dado> arrayList_dadosActivados;
-    private ArrayList <Dado> arrayList_dadosInactivos;
-    private ArrayList <Dado> arrayList_dadosUtilizados;
+    private ArrayList<Dado> arrayList_dadosActivados;
+    private ArrayList<Dado> arrayList_dadosInactivos;
+    private ArrayList<Dado> arrayList_dadosUtilizados;
     private boolean terminarPartida;
-    private int dadosAccionar,numeroDragon, numero42, puntaje;
+    private int dadosAccionar, numeroDragon, numero42, puntaje;
 
 
     /**
@@ -42,16 +42,16 @@ public class ModelClass {
         arrayList_dadosUtilizados = new ArrayList<>();
         iniciar_arrayList_dadosUtilizados();
 
-        numero42=0;
-        numeroDragon=0;
-        dadosAccionar=0;
-        puntaje=0;
-        terminarPartida=false;
+        numero42 = 0;
+        numeroDragon = 0;
+        dadosAccionar = 0;
+        puntaje = 0;
+        terminarPartida = false;
 
 
     }
 
-    public void iniciar_arrayList_dadosActivados(){
+    public void iniciar_arrayList_dadosActivados() {
 
         arrayList_dadosActivados.add(dado1);
         arrayList_dadosActivados.add(dado2);
@@ -63,7 +63,7 @@ public class ModelClass {
 
     }
 
-    public void iniciar_arrayList_dadosInactivos(){
+    public void iniciar_arrayList_dadosInactivos() {
 
         arrayList_dadosInactivos.add(dado8);
         arrayList_dadosInactivos.add(dado9);
@@ -71,70 +71,75 @@ public class ModelClass {
 
     }
 
-    public void iniciar_arrayList_dadosUtilizados(){}
+    public void iniciar_arrayList_dadosUtilizados() {
+    }
 
 
     public ArrayList<Dado> getDadosActivados() {
         return arrayList_dadosActivados;
     }
 
-    public ArrayList<Dado>  getDadosInactivos() { return arrayList_dadosInactivos; }
+    public ArrayList<Dado> getDadosInactivos() {
+        return arrayList_dadosInactivos;
+    }
 
-    public ArrayList<Dado> getDadosUtilizados() { return arrayList_dadosUtilizados; }
+    public ArrayList<Dado> getDadosUtilizados() {
+        return arrayList_dadosUtilizados;
+    }
 
-    public void eliminarElementoDeArrayDadosInactivos(){
+    public void eliminarElementoDeArrayDadosInactivos() {
 
         arrayList_dadosInactivos.remove(0);
 
     }
 
-    public void eliminarElementoDeArrayDadosActivados(int posicionDelButton){
+    public void eliminarElementoDeArrayDadosActivados(int posicionDelButton) {
 
         arrayList_dadosActivados.remove(posicionDelButton);
 
     }
 
-    public void meterEnArrayDadosUtilizados(Dado x){
+    public void meterEnArrayDadosUtilizados(Dado x) {
         arrayList_dadosUtilizados.add(x);
     }
 
-    public void verificarPartida(){
-        if(!arrayList_dadosActivados.isEmpty()){
+    public void verificarPartida() {
+        if (!arrayList_dadosActivados.isEmpty()) {
             for (Dado elementoDeLista : arrayList_dadosActivados) {
                 if (elementoDeLista.getCara() == 1 || elementoDeLista.getCara() == 2 || elementoDeLista.getCara() == 3 ||
-                        ((elementoDeLista.getCara() == 5) && !arrayList_dadosInactivos.isEmpty()) ) {
+                        ((elementoDeLista.getCara() == 5) && !arrayList_dadosInactivos.isEmpty())) {
                     dadosAccionar++;
                 }
-                if(elementoDeLista.getCara()==4){
+                if (elementoDeLista.getCara() == 4) {
                     numeroDragon++;
                 }
             }
         }
 
-        if(dadosAccionar==0){
-            terminarPartida=true;
+        if (dadosAccionar == 0) {
+            terminarPartida = true;
         }
 
-        if(arrayList_dadosActivados.size()==1){
-            if(arrayList_dadosActivados.get(0).getCara()== 1||
-                    arrayList_dadosActivados.get(0).getCara()==2 ||
-                    arrayList_dadosActivados.get(0).getCara()==3){
-                terminarPartida=true;
+        if (arrayList_dadosActivados.size() == 1) {
+            if (arrayList_dadosActivados.get(0).getCara() == 1 ||
+                    arrayList_dadosActivados.get(0).getCara() == 2 ||
+                    arrayList_dadosActivados.get(0).getCara() == 3) {
+                terminarPartida = true;
             }
         }
 
-        if(terminarPartida==true){
-            if(numeroDragon>0){
-                puntaje=0;
+        if (terminarPartida == true) {
+            if (numeroDragon > 0) {
+                puntaje = 0;
             }
 
-            for(Dado elemento:arrayList_dadosActivados){
-                if(elemento.getCara()==6){
+            for (Dado elemento : arrayList_dadosActivados) {
+                if (elemento.getCara() == 6) {
                     numero42++;
-                    if(numero42==1){
-                        puntaje+=1;
-                    }else {
-                        puntaje+=numero42;
+                    if (numero42 == 1) {
+                        puntaje += 1;
+                    } else {
+                        puntaje += numero42;
                     }
                 }
             }
@@ -155,26 +160,26 @@ public class ModelClass {
         }
     }
 
-    public  void accionarBoton(int posicionArrayDadosActivados){
+    public void accionarBoton(int posicionArrayDadosActivados) {
 
         switch (arrayList_dadosUtilizados.get(arrayList_dadosUtilizados.size() - 1).getCara()) {
             case 1 -> {
-               arrayList_dadosActivados.remove(posicionArrayDadosActivados);
-               Dado nuevoDadito = new Dado();
-               arrayList_dadosActivados.add(posicionArrayDadosActivados,nuevoDadito);
+                arrayList_dadosActivados.remove(posicionArrayDadosActivados);
+                Dado nuevoDadito = new Dado();
+                arrayList_dadosActivados.add(posicionArrayDadosActivados, nuevoDadito);
 
 
             }
             case 2 -> meterEnArrayDadosUtilizados(arrayList_dadosActivados.get(posicionArrayDadosActivados));
             case 3 -> {
                 arrayList_dadosActivados.get(posicionArrayDadosActivados).setCara(arrayList_dadosActivados.get(posicionArrayDadosActivados).getCaraOpuesta());
-                System.out.println("esta es la cara del nuevo dado luego de girar: "+arrayList_dadosActivados.get(posicionArrayDadosActivados).getCara());
+                System.out.println("esta es la cara del nuevo dado luego de girar: " + arrayList_dadosActivados.get(posicionArrayDadosActivados).getCara());
                 System.out.println("cara opuesta del nuevo dado: " + arrayList_dadosActivados.get(posicionArrayDadosActivados).getCaraOpuesta());
-                System.out.println("nombre del dado: "+ arrayList_dadosActivados.get(posicionArrayDadosActivados).getNameFace());
+                System.out.println("nombre del dado: " + arrayList_dadosActivados.get(posicionArrayDadosActivados).getNameFace());
             }
             case 5 -> {
                 Dado nuevoDado = new Dado();
-                arrayList_dadosActivados.add(posicionArrayDadosActivados,nuevoDado);
+                arrayList_dadosActivados.add(posicionArrayDadosActivados, nuevoDado);
                 eliminarElementoDeArrayDadosInactivos();
                 System.out.println("activaste un dado");
 
@@ -185,6 +190,10 @@ public class ModelClass {
             }
         }
 
+    }
+
+    public void ramas(){
+        int x=19;
     }
 }
 //
